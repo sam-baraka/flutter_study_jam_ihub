3329834c4d1f8b02f66a6267d0968f1f60037dc5
sababuvercetti <sababuvercetti@gmail.com>
2019-07-21 14:31:00 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/vercetti/flutter_study_jam_ihub/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.vercetti.flutter_study_jam_ihub
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
